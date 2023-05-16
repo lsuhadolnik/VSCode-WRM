5309af86d2ebe236831a56f37bca14e3546eb439
Lovro Suhadolnik <lovros16@gmail.com>
2023-05-16 18:51:27 +0200

    Loaded demo

--- src/extension.ts
+++ src/extension.ts
@@ -1,45 +1,50 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+'use strict';
+
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+import { DepNodeProvider, Dependency } from './nodeDependencies';
+import { JsonOutlineProvider } from './jsonOutline';
+import { FtpExplorer } from './ftpExplorer';
+import { FileExplorer } from './fileExplorer';
+import { TestViewDragAndDrop } from './testViewDragAndDrop';
+import { TestView } from './testView';
+
 export function activate(context: vscode.ExtensionContext) {
+	const rootPath = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0))
+		? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "wrmgr" is now active!');
+	// Samples of `window.registerTreeDataProvider`
+	const nodeDependenciesProvider = new DepNodeProvider(rootPath);
+	vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
+	vscode.commands.registerCommand('nodeDependencies.refreshEntry', () => nodeDependenciesProvider.refresh());
+	vscode.commands.registerCommand('extension.openPackageOnNpm', moduleName => vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)));
+	vscode.commands.registerCommand('nodeDependencies.addEntry', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
+	vscode.commands.registerCommand('nodeDependencies.editEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
+	vscode.commands.registerCommand('nodeDependencies.deleteEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('wrmgr.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from WRMGR!');
+	const jsonOutlineProvider = new JsonOutlineProvider(context);
+	vscode.window.registerTreeDataProvider('jsonOutline', jsonOutlineProvider);
+	vscode.commands.registerCommand('jsonOutline.refresh', () => jsonOutlineProvider.refresh());
+	vscode.commands.registerCommand('jsonOutline.refreshNode', offset => jsonOutlineProvider.refresh(offset));
+	vscode.commands.registerCommand('jsonOutline.renameNode', args => {
+		let offset = undefined;
+		if (args.selectedTreeItems && args.selectedTreeItems.length) {
+			offset = args.selectedTreeItems[0];
+		} else if (typeof args === 'number') {
+			offset = args;
+		}
+		if (offset) {
+			jsonOutlineProvider.rename(offset);
+		}
 	});
+	vscode.commands.registerCommand('extension.openJsonSelection', range => jsonOutlineProvider.select(range));
 
-	const treeDataProvider = new MyTreeDataProvider(); // Your custom data provider for the sidebar
-const view = vscode.window.createTreeView('mySidebarView', {
-  treeDataProvider: treeDataProvider as any,
-  showCollapseAll: true,
-});
+	// Samples of `window.createView`
+	new FtpExplorer(context);
+	new FileExplorer(context);
 
-	context.subscriptions.push(disposable);
+	// Test View
+	new TestView(context);
+
+	new TestViewDragAndDrop(context);
 }
-
-class MyTreeDataProvider {
-	public onDidChangeTreeData: any = new vscode.EventEmitter();
-	
-	getChildren(element: any) {
-	  // Return the child elements for the given parent element
-	  // If `element` is null, return the root level elements
-	}
-	
-	getTreeItem(element: any) {
-	  // Return a `TreeItem` object representing the provided element
-	}
-  }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
