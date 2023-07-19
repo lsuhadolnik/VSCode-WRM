b0184c9b1bbf18f697bf567f98a6949e07e42a14
Lovro Suhadolnik <lovros16@gmail.com>
2023-07-19 06:00:34 +0200

    Added jest

---
+++ tests/auth.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "@jest/globals";
+
+describe("Authentication tests", () => {
+  it("should allow me to auth", () => {
+    expect("Hello").toEqual("a");
+  });
+});
